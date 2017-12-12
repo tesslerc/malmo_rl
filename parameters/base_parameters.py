0701138147e8942691c6f3c83f6b2bd55a1f8ccb
Chen Tessler <chen.tessler@gmail.com>
2017-12-12 08:33:54 +0200

    Default actions no longer contain 'attack'. Otherwise the agent will break the environment.

--- parameters/base_parameters.py
+++ parameters/base_parameters.py
@@ -8,8 +8,8 @@ parser.add_argument('--malmo_ports', type=int, nargs='+', default=None,
 parser.add_argument('--number_of_agents', type=int, default=1,
                     help='Number of agents to run in parallel, all use the same policy. This is used to increase the number of observation samples obtained over time.')
 parser.add_argument('--max_steps', type=int, default=2000000)
-parser.add_argument('--eval_frequency', type=int, default=200)
-parser.add_argument('--eval_steps', type=int, default=50)
+parser.add_argument('--eval_frequency', type=int, default=1000)
+parser.add_argument('--eval_steps', type=int, default=150)
 parser.add_argument('--image_width', type=int, default=84)
 parser.add_argument('--image_height', type=int, default=84)
 # TODO: Add support in models for RGB format.
@@ -20,5 +20,5 @@ parser.add_argument('--graph_moving_average_length', default=5, type=int,
                     help='Graphs plotted using a moving average over the last N points.')
 parser.add_argument('--verbose_prints', default=False, action='store_true',
                     help='Enable verbose debug prints for more informative details.')
-parser.add_argument('--available_actions', default=['move 1', 'turn -1', 'turn 1', 'attack 1'], nargs='+',
+parser.add_argument('--available_actions', default=['move 1', 'turn -1', 'turn 1'], nargs='+',
                     help='Space separated list of available actions. E.g. "\'move 1\' \'turn -1\'..."')
