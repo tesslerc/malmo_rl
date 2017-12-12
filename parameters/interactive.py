a6ce0b13965e71e750649df75a30d410ae46c4e3
Chen Tessler <chen.tessler@gmail.com>
2017-12-12 08:36:53 +0200

    Interactive crashed due to non-existent flags. This is a dirty workaround but the interactive 'agent' is only for testing.

--- parameters/interactive.py
+++ parameters/interactive.py
@@ -1,3 +1,4 @@
 from parameters.base_parameters import parser as base_parser
 
 parser = base_parser
+parser.add_argument('--learn_start', type=int, default=100000)  # Need some value, not really relevant.
