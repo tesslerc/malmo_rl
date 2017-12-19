b0d6c63e37c1ea92e57f6b0308cf8a60316dc1b2
Chen Tessler <chen.tessler@gmail.com>
2017-12-19 08:11:25 +0200

    Allow for configurable state size, i.e number of previous observations (images) which build up the current state.

--- policies/models/distributional_dqn.py
+++ policies/models/distributional_dqn.py
@@ -8,20 +8,17 @@ class DISTRIBUTIONAL_DQN(nn.Module):
     Bellamare et al. (2017) - https://arxiv.org/abs/1707.06887
     """
 
-    def __init__(self, n_action: int, n_atoms: int) -> None:
+    def __init__(self, n_action: int, n_atoms: int, state_size: int) -> None:
         super(DISTRIBUTIONAL_DQN, self).__init__()
         self.n_action = n_action
         self.n_atoms = n_atoms
 
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, padding=0)  # (In Channel, Out Channel, ...)
+        self.conv1 = nn.Conv2d(state_size, 32, kernel_size=8, stride=4, padding=0)  # (In Channel, Out Channel, ...)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
 
         self.affine1 = nn.Linear(3136, 512)
-
-        self.affine2 = []
-        for _ in range(self.n_action):
-            self.affine2.append(nn.Linear(512, self.n_atoms))
+        self.affine2 = [nn.Linear(512, self.n_atoms) for _ in range(self.n_action)]
 
     def forward(self, x, atom_values):
         h = F.relu(self.conv1(x))
@@ -30,12 +27,12 @@ class DISTRIBUTIONAL_DQN(nn.Module):
 
         h = F.relu(self.affine1(h.view(h.size(0), -1)))
 
-        distributions = []
+        distribution_list = []
         for idx in range(self.n_action):
             # Each action 'a' outputs a discrete n_atoms distribution over Q(s,a).
-            atoms = self.affine2[idx](h).unsqueeze(1)
-            distributions.append(F.softmax(atoms, dim=2))
-        distributions = torch.cat(distributions, 1)
+            atoms = self.affine2[idx](h).unsqueeze(1)  # Add the action dimension.
+            distribution_list.append(F.softmax(atoms, dim=-1))
+        distributions = torch.cat(distribution_list, 1)
 
         # Returns Q(s, a) probabilities and E[Q(s, a)]
         return distributions, distributions @ atom_values
--- policies/models/dqn.py
+++ policies/models/dqn.py
@@ -7,11 +7,11 @@ class DQN(nn.Module):
     Mnih et al. (2013) - https://arxiv.org/abs/1312.5602
     """
 
-    def __init__(self, n_action: int):
+    def __init__(self, n_action: int, state_size: int):
         super(DQN, self).__init__()
         self.n_action = n_action
 
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, padding=0)  # (In Channel, Out Channel, ...)
+        self.conv1 = nn.Conv2d(state_size, 32, kernel_size=8, stride=4, padding=0)  # (In Channel, Out Channel, ...)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
 
@@ -22,7 +22,6 @@ class DQN(nn.Module):
         h = F.relu(self.conv1(x))
         h = F.relu(self.conv2(h))
         h = F.relu(self.conv3(h))
-
         h = F.relu(self.affine1(h.view(h.size(0), -1)))
         h = self.affine2(h)
         return h
