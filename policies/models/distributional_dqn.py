f84797acdb757c9e80a4765917dcfa0b63d91b0b
Chen Tessler <chen.tessler@gmail.com>
2017-12-26 15:02:37 +0200

    Refactoring.

--- policies/models/distributional_dqn.py
+++ policies/models/distributional_dqn.py
@@ -1,35 +1,24 @@
 import torch
-from torch import nn
 from torch.nn import functional as F
 
+from policies.models.dqn import DQN
 
-class DISTRIBUTIONAL_DQN(nn.Module):
+
+class DISTRIBUTIONAL_DQN(DQN):
     """Implements a 3 layer convolutional network with 2 fully connected layers at the end as explained by:
     Bellamare et al. (2017) - https://arxiv.org/abs/1707.06887
     """
 
     def __init__(self, n_action: int, n_atoms: int, state_size: int) -> None:
-        super(DISTRIBUTIONAL_DQN, self).__init__()
+        super(DISTRIBUTIONAL_DQN, self).__init__(n_atoms * n_action, state_size)
         self.n_action = n_action
         self.n_atoms = n_atoms
 
-        self.conv1 = nn.Conv2d(state_size, 32, kernel_size=8, stride=4, padding=0)  # (In Channel, Out Channel, ...)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
-
-        self.affine1 = nn.Linear(3136, 512)
-        self.affine2 = [nn.Linear(512, self.n_atoms) for _ in range(self.n_action)]
-
-        self.log_softmax = nn.LogSoftmax(dim=-1)
-
     def forward(self, x):
-        h = F.relu(self.conv1(x))
-        h = F.relu(self.conv2(h))
-        h = F.relu(self.conv3(h))
-
-        h = F.relu(self.affine1(h.view(h.size(0), -1)))
+        output = super(DISTRIBUTIONAL_DQN, self).forward(x)
 
         # Returns Q(s, a) probabilities and E[Q(s, a)]
-        distribution_list = [F.softmax(affine2(h), dim=1) for affine2 in self.affine2]
-        distributions = torch.stack(distribution_list, 1)
-        return distributions
+        # Probabilities with action over second dimension
+        probs = torch.stack([F.softmax(p, dim=1) for p in output.chunk(self.n_action, 1)], 1)
+
+        return probs.clamp(min=1e-8, max=1 - 1e-8)  # Use clipping to prevent NaNs
--- policies/models/dqn.py
+++ policies/models/dqn.py
@@ -3,7 +3,7 @@ from torch import nn
 
 class Flatten(nn.Module):
     def forward(self, x):
-        x = x.view(x.size()[0], -1)
+        x = x.view(x.size(0), -1)
         return x
 
 
