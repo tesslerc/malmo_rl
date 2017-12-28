27941ae02a445a0d4bed1f0eae2ba9730a056484
Chen Tessler <chen.tessler@gmail.com>
2017-12-28 22:26:43 +0200

    Added argument to disable cuda

--- parameters/base_parameters.py
+++ parameters/base_parameters.py
@@ -12,7 +12,6 @@ parser.add_argument('--eval_frequency', type=int, default=1000)
 parser.add_argument('--eval_steps', type=int, default=100)
 parser.add_argument('--image_width', type=int, default=84)
 parser.add_argument('--image_height', type=int, default=84)
-# TODO: Add support in models for RGB format.
 parser.add_argument('--retain_rgb', default=False, action='store_true')
 parser.add_argument('--no_visualization', default=False, action='store_true',
                     help='When flag exists, will not plot visualizations.')
@@ -26,5 +25,6 @@ parser.add_argument('--ms_per_tick', default=100, type=int,
                     help='Delay between ticks, this is a setting for the Malmo simulator.')
 parser.add_argument('--checkpoint_interval', type=int, default=5000)
 parser.add_argument('--save_name', type=str, help='The filename given to the saved checkpoint.')
+parser.add_argument('--no_cuda', default=False, action='store_true', help='When flag exists, cuda will not be used.')
 parser.add_argument('--resume', default=False, action='store_true',
                     help='Resume training of previous model based on save_name.')
