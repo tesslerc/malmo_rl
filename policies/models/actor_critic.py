0bfce54f2b980cc8ddc0f0cc3c8dda83454da3f0
Chen Tessler <chen.tessler@gmail.com>
2018-02-26 09:12:03 +0200

    Rename state_size to hist_len. Split policy-value at a higher place in the network.

--- policies/models/actor_critic.py
+++ policies/models/actor_critic.py
@@ -9,29 +9,33 @@ class Flatten(nn.Module):
 
 
 class ACTOR_CRITIC(nn.Module):
-    def __init__(self, n_action: int, state_size: int, value_projection_outputs: int =1):
+    def __init__(self, n_action: int, hist_len: int):
         super(ACTOR_CRITIC, self).__init__()
         self.n_action = n_action
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(state_size, 32, kernel_size=8, stride=4, padding=0),  # (In Channel, Out Channel, ...)
+            nn.Conv2d(hist_len, 32, kernel_size=8, stride=4, padding=0),  # (In Channel, Out Channel, ...)
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            Flatten(),
-            nn.Linear(3136, 512),
-            nn.ReLU()
+            Flatten()
         )
 
-        self.v_projection = nn.Linear(512, self.n_action * value_projection_outputs)
-        self.policy_projection = nn.Linear(512, self.n_action)
+        self.v_projection = nn.Sequential(
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+        self.policy_projection = nn.Sequential(
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, self.n_action)
+        )
 
         self.softmax = nn.Softmax(dim=1)
         self.log_softmax = nn.LogSoftmax(dim=1)
-
-        self.train()
 
     def forward(self, inputs):
         x = self.feature_extractor(inputs)
@@ -40,19 +44,5 @@ class ACTOR_CRITIC(nn.Module):
         policy = self.softmax(logits)
         log_policy = self.log_softmax(logits)
 
-        Q = self.v_projection(x)
-        V = (Q * policy).sum(1).unsqueeze(-1)
+        V = self.v_projection(x)
         return policy, log_policy, V
-
-
-class DISTRIBUTIONAL_ACTOR_CRITIC(ACTOR_CRITIC):
-    def __init__(self, n_action: int, n_atoms: int, state_size: int):
-        super(DISTRIBUTIONAL_ACTOR_CRITIC, self).__init__(n_action, state_size, n_atoms * n_action)
-        self.n_atoms = n_atoms
-
-    def forward(self, x):
-        policy, log_policy, flat_quantiles, lstm_state = super(DISTRIBUTIONAL_ACTOR_CRITIC, self).forward(x)
-        quantiles = torch.stack([p for p in flat_quantiles.chunk(self.n_action, 1)], 1)
-
-        expected_quantiles = quantiles * policy.unsqueeze(-1)
-        return policy, log_policy, expected_quantiles.sum(1), lstm_state
